b0561e8c18c281c04076dcb83c0ec2cfa5172c53
Micah Effiong <micaiah.effiong@gmail.com>
2023-05-25 00:36:55 +0100

    rename folders and add new font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        sora: [
-          'Sora-Bold',
-          'Sora-ExtraBold',
-          'Sora-ExtraLight',
-          'Sora-Light',
-          'Sora-Medium',
-          'Sora-Regular',
-          'Sora-SemiBold',
-          'Sora-Thin',
-        ],
+        montserrat: ["Montserrat"],
       },
     },
   },
